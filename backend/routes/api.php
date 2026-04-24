48be147cdc907a757a25c04cb53c3f4a73a5abf9
Theark86 <palmieroarcangelo@gmail.com>
2026-04-24 17:16:09 +0200

    Aggiunta Backend Routes/Api

---
+++ backend/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+require_once("../config/db.php");
+require_once("../controllers/bookController.php");
+
+header("Content-Type: application/json");
+
+$action = $_GET["action"] ?? "";
+
+switch ($action) {
+
+    case "books":
+        echo json_encode(BookController::getAllBooks($conn));
+        break;
+
+    case "book":
+        if (isset($_GET["id"])) {
+            echo json_encode(BookController::getBookById($conn, $_GET["id"]));
+        } else {
+            echo json_encode(["error" => "ID mancante"]);
+        }
+        break;
+
+    default:
+        echo json_encode(["error" => "Azione non valida"]);
+}
+?>
